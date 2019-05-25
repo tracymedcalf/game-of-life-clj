26b77ac7f3e8c96c378a275327bf468b44694bad
tracymedcalf <mtracy_22@yahoo.com>
2019-05-24 21:00:28 -0700

    about to branch then make major changes

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -26,21 +26,41 @@
 ; (defn xy-to-index [width x y]
 ;   (+ x (* y width)))
 
+; (defn index-in-bounds [col indices]
+;   (loop [indices indices acc []]
+;     (when (seq indices)
+;       (recur (rest indices)
+;       (if (cons (nth col (first indices)) acc)))))
+;
+(defn index-in-bounds [col indices]
+  (for [index indices
+    :when (some? (get col index))] (get col index)))
+; (index-in-bounds ["a" "b" "c"] [0 1 5])
+
 (defn dead>alive [neighbor's-indices world]
-  (if (> (apply + (mapv world neighbor's-indices))
-   (count neighbor's-indices)) 1 0))
+  (let [neighbors (index-in-bounds neighbor's-indices world)]
+    (if (> (apply + neighbors)
+     (count neighbors)) 1 0)))
+
+; (dead>alive [0 8 9 10] [1 0 1 0 1 0])
+
+; (defn new-world [old-world width height]
+;  (for [state old-world
+;        cell (cartesian-product (range width) (range height))]
+;  (let [xy-to-index (fn [x y] (+ x (* y width)))]
+;    (dead>alive
+;      (map #(apply xy-to-index %) (apply neighbors'-xy's cell)) old-world))))
+;
+; (let [xy-to-index (fn [x y] (+ x (* y 10)))]
+; (dead>alive (map #(apply xy-to-index %)
+; (apply neighbors'-xy's [5 7])) [0 1 1, 1 1 0, 0 0 0]))
 
 (defn new-world [old-world width height]
- (for [state old-world
-       cell (cartesian-product (range width) (range height))]
- (let [xy-to-index (fn [x y] (+ x (* y width)))]
-   (dead>alive
-     (map #(apply xy-to-index %) (apply neighbors'-xy's cell)) old-world))))
+(for [state old-world cell
+  (cartesian-product (range width) (range height))]
+   [state cell]))
 
- (let [xy-to-index (fn [x y] (+ x (* y 10)))]
-(dead>alive (map #(apply xy-to-index %) (apply neighbors'-xy's [5 7])) [0 1 1, 1 1 0, 0 0 0]))
-
-(new-world 3 3 (0 1 1, 1 1 0, 0 0 0))
+(new-world (0 1 1, 1 1 0, 0 0 0) 3 3)
 
 (defn -main
   "I don't do a whole lot ... yet."
