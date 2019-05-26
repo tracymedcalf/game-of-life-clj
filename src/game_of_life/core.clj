3e6b38da8e6a05079337ad85dc680fb6baa92620
tracymedcalf <mtracy_22@yahoo.com>
2019-05-26 14:39:33 -0700

    game works

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -2,72 +2,48 @@
   (:gen-class))
 
 (defn random-world [size]
-  (take size (repeatedly #(rand-int 2))))
+  (vec (take size (repeatedly #(rand-int 2)))))
 
 (defn input [msg]
   (println msg) (read-line))
 
 (defn str->int [s]
   (Integer. (re-find #"[0-9]*" s)))
-; (str->int "778\n")
 
 (def int-input (comp str->int input))
 
-(defn cartesian-product [vec1 vec2]
-  (for [x vec1 y vec2] [x y]))
-; (cartesian-product [1 2 3] ['a 'b 'c])
-
-(defn neighbors'-xy's [x y]
-  (cartesian-product
-    [(dec x) x (inc x)]
-    [(dec y) y (dec y)]))
-; (neighbors'-xy's 5 6)
-
-; (defn xy-to-index [width x y]
-;   (+ x (* y width)))
-
-; (defn index-in-bounds [col indices]
-;   (loop [indices indices acc []]
-;     (when (seq indices)
-;       (recur (rest indices)
-;       (if (cons (nth col (first indices)) acc)))))
-;
-(defn index-in-bounds [col indices]
+(defn indices-in-bounds [col indices]
+  (assert (vector? col))
   (for [index indices
-    :when (some? (get col index))] (get col index)))
-; (index-in-bounds ["a" "b" "c"] [0 1 5])
+    :when (some? (get col index))] index))
 
-(defn dead>alive [neighbor's-indices world]
-  (let [neighbors (index-in-bounds neighbor's-indices world)]
+(defn dead>alive [neighbors'-indices world]
+  (let [neighbors (mapv world (indices-in-bounds world neighbors'-indices))]
     (if (> (apply + neighbors)
-     (count neighbors)) 1 0)))
+     (/ (count neighbors) 2)) 1 0)))
 
-; (dead>alive [0 8 9 10] [1 0 1 0 1 0])
 
-; (defn new-world [old-world width height]
-;  (for [state old-world
-;        cell (cartesian-product (range width) (range height))]
-;  (let [xy-to-index (fn [x y] (+ x (* y width)))]
-;    (dead>alive
-;      (map #(apply xy-to-index %) (apply neighbors'-xy's cell)) old-world))))
-;
-; (let [xy-to-index (fn [x y] (+ x (* y 10)))]
-; (dead>alive (map #(apply xy-to-index %)
-; (apply neighbors'-xy's [5 7])) [0 1 1, 1 1 0, 0 0 0]))
+(defn neighbors'-indices [width i]
+  [(- i width 1) (- i width) (- i width -1)
+   (- i 1) i (+ i 1)
+   (+ i width -1) (+ i width) (+ i width 1)])
 
-(defn new-world [old-world width height]
-(for [state old-world cell
-  (cartesian-product (range width) (range height))]
-   [state cell]))
+(defn new-world [neighbors'-indices old-world indices]
+  (mapv #(dead>alive (neighbors'-indices %) old-world) indices))
 
-(new-world (0 1 1, 1 1 0, 0 0 0) 3 3)
+(defn print-grid [coll width]
+  (loop [coll coll]
+    (when (seq coll) (println (take width coll))
+    (recur (drop width coll)))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [width (int-input "width?")
-        height (int-input "height?")]
-        (let [size (* width height)]
-        (new-world
-           (random-world size)
-           width height))))
+        size (* width (int-input "height?"))]
+    (loop [world (random-world size) user-input ""]
+      (if (not= "q" user-input)
+      (do (println "'q<enter>' to quit") (print-grid world width)
+        (recur (new-world #(neighbors'-indices width %) world (range size))
+         (read-line)))))))
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -5,3 +5,35 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+
+; (print-grid (new-world #(neighbors'-indices 9 %)
+;                                      [1 1 1 1 0 0 0 0 0
+;                                       1 0 1 0 0 0 0 0 0
+;                                       0 0 0 0 0 0 0 0 0
+;                                       1 1 1 1 1 1 1 1 1
+;                                       1 1 1 0 0 0 0 0 0
+;                                       1 1 1 1 0 1 0 0 0
+;                                       1 1 1 1 0 0 0 1 0] (range 63)) 9)
+;
+; (print-grid (new-world #(neighbors'-indices 9 %)
+;                                      '(1 1 1 1 0 0 0 0 0
+;                                       1 0 1 0 0 0 0 0 0
+;                                       0 0 0 0 0 0 0 0 0
+;                                       1 1 1 1 1 1 1 1 1
+;                                       1 1 1 0 0 0 0 0 0
+;                                       1 1 1 1 0 1 0 0 0
+;                                       1 1 1 1 0 0 0 1 0) (range 63)) 9)
+; (print-grid (new-world #(neighbors'-indices 9 %)
+;   (random-world 63) (range 63)) 9)
+;
+; (random-world 63)
+
+; (random-world 9)
+; (str->int "778\n")
+; (indices-in-bounds [0 0 0 0 0 0 0 0 0] (neighbors'-indices 3 7))
+; (indices-in-bounds [0 1 2 3 4 5] [-1 0 1 9 10])
+; (dead>alive (neighbors'-indices 3 7) [0 0 0 0 1 1 1 1 1])
+; (dead>alive (neighbors'-indices 3 5) [1 1 0 0 0 0 0 0 0])
+; (neighbors'-indices 3 5)
+; (print-grid [0 1 2 3 4 5 6 7 8 ] 3)
